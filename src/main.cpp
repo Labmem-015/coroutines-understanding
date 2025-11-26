b7b9aefb2a0f13ebfd75713fd9faaf81278c4341
Vladlen Naumov <v.naumov@avsw.ru>
2025-11-26 20:23:42 +0300

    Basic structure of project

---
+++ src/main.cpp
@@ -0,0 +1,4 @@
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
---
+++ src/main.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstdlib>
+#include <cstdint>
+
+#include <coroutine>
+#include <string>
+
+#include <iostream>
