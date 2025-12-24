b6ffcb64ba9175117e8c9ddd0dce6a5642f2de6c
Vladlen Naumov <v.naumov@avsw.ru>
2025-12-24 11:31:28 +0300

    WIP: completed main work. There's some compile errors left

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include "awaitable.hpp"
 #include "simple_coroutine.hpp"
 
-void foo_coroutine() {
+using namespace simple_coro;
+
+Task<std::string> foo_coroutine() {
     // call async I/O function here
     // co_await ...;
     std::cout << "This is couroutine" << std::endl;
-    co_return;
+    co_return std::string("End of coroutine!");
 }
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
-    foo_coroutine();
+    auto task = foo_coroutine();
+    std::cout << "Coroutine result: " << task.get_value() << std::endl;
     std::cout << "The end" << std::endl;
     return EXIT_SUCCESS;
 }
--- src/simple_coroutine.hpp
+++ src/simple_coroutine.hpp
@@ -2,10 +2,30 @@
 
 namespace simple_coro {
 
-struct task {
+template<typename T>
+struct Task {
+    struct promise_type;
+    using coro_handle_t = std::couroutine_handle<promise_type>;
     struct promise_type {
-        
+        T value;
+        auto get_return_object() {return coro_handle_t::from_promise(*this) ;}
+        std::suspend_always initial_suspend() noexcept { return {};}
+        std::suspend_always final_suspend() noexcept { return {};}
+        void return_value(T value) { this->value = value; }
+        void unhandled_exception() { std::cout << "Exception here" << std::endl; }
     };
+    Task(coro_handle_t handle) : coroutine_h(handle) {
+        std::cout << "task created!" << std::endl;
+    }
+    ~Task() {
+        if (coroutine_h) {
+            coroutine_h.destroy();
+        }
+    }
+    coro_handle_t coroutine_h;
+    T get_value() { 
+        coroutine_h.promise().value;
+    }
 };
     
 } // namespace simple_coro
